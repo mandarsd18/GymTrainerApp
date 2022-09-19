39d9371136e0122335bec7416046a5423708e43b
mandarsd18 <113833034+mandarsd18@users.noreply.github.com>
2022-09-19 15:40:01 +0530

    gymTrainerApp

---
+++ app/src/main/java/com/example/gymtrainer/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.gymtrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton imageButton1;
+    private ImageButton imageButton2;
+    private ImageButton imageButton3;
+    private ImageButton imageButton4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageButton1=findViewById(R.id.imageButton1);
+        imageButton2=findViewById(R.id.imageButton2);
+        imageButton3=findViewById(R.id.imageButton3);
+        imageButton4=findViewById(R.id.imageButton4);
+
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this, MainActivity4.class);
+                startActivity(intent);
+            }
+        });
+
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this, MainActivity5.class);
+                startActivity(intent);
+            }
+        });
+
+        }
+    }
+
